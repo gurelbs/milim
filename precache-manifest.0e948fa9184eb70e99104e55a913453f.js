b57b512960a063b0f410d4ad80064f2b0b0f3e69
gurelbs <guri240@gmail.com>
2020-10-23 13:18:26 +0300

    Updates

---
+++ precache-manifest.0e948fa9184eb70e99104e55a913453f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31d6a6d7c8ebe7af892153ccea140b67",
+    "url": "/milim/index.html"
+  },
+  {
+    "revision": "92b8253ae219ee902e8e",
+    "url": "/milim/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "92b8253ae219ee902e8e",
+    "url": "/milim/static/js/2.da0597e0.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/milim/static/js/2.da0597e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f8196f4dbcecc9b66a2",
+    "url": "/milim/static/js/main.cb1fe2da.chunk.js"
+  },
+  {
+    "revision": "d07370c5a64777ecb7aa",
+    "url": "/milim/static/js/runtime-main.3e5f1e26.js"
+  }
+]);
